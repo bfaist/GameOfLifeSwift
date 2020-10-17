5a9d5fd74732ce6bee092409647ad8b5e561cd04
Bob Faist <bob.faist@gmail.com>
2020-10-16 23:09:23 -0400

    Initial Commit

---
+++ GameOfLife/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GameOfLife
+//
+//  Created by Robert Faist on 10/16/20.
+//  Copyright © 2020 Robert Faist. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
